45150a8cb452072433fe070bd5d8065e794c3733
kirilllzaitsev <kzaitsev88@gmail.com>
2026-01-18 21:35:59 +0100

    Inference m

--- notebook/inference.py
+++ notebook/inference.py
@@ -129,6 +129,10 @@ class Inference:
         pointmap=None,
         event_image=None,
         decode_formats=['gaussian'],
+        use_stage1_distillation=False,
+        stage1_inference_steps=None,
+        use_stage2_distillation=False,
+        stage2_inference_steps=None
     ) -> dict:
         image = self.merge_mask_to_rgba(image, mask)
         return self._pipeline.run(
@@ -140,10 +144,13 @@ class Inference:
             with_texture_baking=False,
             with_layout_postprocess=True,
             use_vertex_color=True,
-            stage1_inference_steps=None,
             pointmap=pointmap,
             decode_formats=decode_formats,
             event_image=event_image,
+            stage1_inference_steps=stage1_inference_steps,
+            use_stage1_distillation=use_stage1_distillation,
+            stage2_inference_steps=stage2_inference_steps,
+            use_stage2_distillation=use_stage2_distillation,
         )
 
 
